066cd6bee9ae391ce8ecab4707f3505f304b0d23
Thakur <sunny.mca19.du@gmail.com>
2022-01-08 20:34:09 +0530

    Guess My Number game

--- 2. Modal/script.js
+++ 2. Modal/script.js
@@ -24,7 +24,7 @@ for (let i = 0; i < btnOpenModal.length; i++) {
 btnCloseModal.addEventListener("click", closeModal);
 overlay.addEventListener("click", closeModal);
 
-//
+// If escape key is pressed, close modal
 document.addEventListener("keydown", (e) => {
   if (e.key === "Escape" && !modal.classList.contains("hidden")) closeModal();
 });
